78451c8584a217b79053e44ddd89461c6ffde968
Samantha Solomon <sleahsolomon@gmail.com>
2021-02-23 22:28:20 -0500

    Commit 8: Can POST new Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,4 +11,19 @@ class TweetsController < ApplicationController
         render(json: {tweet: tweet})
     end
 
+    #/tweets
+    def create
+        tweet = Tweet.new(tweet_params)
+        if tweet.save
+            render json: {tweet: tweet}
+        else
+            render(status: 422, json: {tweet: tweet, errors: tweet.errors})
+        end
+    end
+
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:title, :content, :author)
+    end
 end
